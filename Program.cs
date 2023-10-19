ba3fd2b011ef11ff7af70d665aca2c1dc9ec1af1
Samuel Chelini <schelini@vmware.com>
2023-10-19 17:42:58 -0400

    initial commit

---
+++ Program.cs
@@ -0,0 +1,23 @@
+using Steeltoe.Bootstrap.Autoconfig;
+
+var builder = WebApplication.CreateBuilder(args).AddSteeltoe();
+
+// Add services to the container.
+builder.Services.AddRazorPages();
+
+var app = builder.Build();
+
+// Configure the HTTP request pipeline.
+if (!app.Environment.IsDevelopment())
+{
+    app.UseExceptionHandler("/Error");
+}
+app.UseStaticFiles();
+
+app.UseRouting();
+
+app.UseAuthorization();
+
+app.MapRazorPages();
+
+app.Run();
